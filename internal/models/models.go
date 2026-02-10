f67c30a463f4fa1d3488881793ec74a7138ff453
Tedi Fibri <tediscript@gmail.com>
2026-02-10 09:02:27 +0700

    feat(models): add data models

---
+++ internal/models/models.go
@@ -0,0 +1,11 @@
+package models
+
+// Models placeholder
+// This file will be expanded with actual data models in future tasks
+
+// User represents a user in the system
+type User struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
